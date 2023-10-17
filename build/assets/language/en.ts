e894a36892f8ea61e1c3885b8a997db011f0b556
Andres Navarrete <andres.navarrete@serverless-solutions.com>
2023-10-16 19:21:43 -0600

    update

---
+++ build/assets/language/en.ts
@@ -0,0 +1,16 @@
+import { ILanguage } from "./language";
+
+export const en: ILanguage = {
+    topNavigation: {
+        contact: "Contact Me",
+        projects: "Projects",
+        knowMe: "Get to know me",
+        blog: "Blog",
+    },
+    contactLinks: {
+        linkedin: "https://www.linkedin.com/in/andres-navarrete-boza-3056b8155/"
+    },
+    mainBanner: {
+        noteOne: ""
+    }
+};
---
+++ build/assets/language/language.ts
@@ -0,0 +1,14 @@
+export interface ILanguage {
+    topNavigation: {
+        contact: string;
+        projects: string;
+        knowMe: string;
+        blog: string;
+    },
+    contactLinks: {
+        linkedin: string;
+    },
+    mainBanner: {
+        noteOne: string;
+    }
+}
