7e7cfbc9227990afb7eed279b0822dcc31fbd7c4
Andres Navarrete <andres.navarrete@serverless-solutions.com>
2024-04-21 13:43:10 -0600

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
-
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-
-import { environment } from 'src/environments/environment';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    // Regarding redux
-    // StoreModule.forRoot(appReducer),
-    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
-    FormsModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
